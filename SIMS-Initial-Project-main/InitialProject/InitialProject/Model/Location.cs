a958b5c8b21f2fd53d3a4b3119dc19655779766d
mil184 <djmilica127@gmail.com>
2023-03-11 00:01:23 +0100

    feat: classes added
    added classes Location and Image

---
+++ SIMS-Initial-Project-main/InitialProject/InitialProject/Model/Location.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InitialProject.Serializer;
+
+namespace InitialProject.Model
+{
+    public class Location : ISerializable
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        public Location(int id, string city, string country)
+        {
+            Id = id;
+            City = city;
+            Country = country;
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues = { Id.ToString(), City, Country };
+            return csvValues;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            Id = Convert.ToInt32(values[0]);
+            City = values[1];
+            Country = values[2];
+        }
+    }
+}
