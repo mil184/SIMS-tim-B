870c3d442943d66a1ee52011c09a96267e51b265
DeyoP <pelemisdejan6@gmail.com>
2023-03-11 15:36:31 +0100

    feat: added guide window outline

---
+++ SIMS-Initial-Project-main/InitialProject/InitialProject/View/Guide/GuideWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using InitialProject.Model;
+using System.Windows;
+
+namespace InitialProject.View.Guide
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class GuideWindow : Window
+    {
+        public User LoggedInUser { get; set; }
+        public GuideWindow(User user)
+        {
+            InitializeComponent();
+            DataContext = this;
+            LoggedInUser = user;
+        }
+    }
+}
