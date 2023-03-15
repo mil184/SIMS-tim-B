19c7a0e991cb22d787629a7903772d8d7091da4c
jelenaa01 <jecasubic@gmail.com>
2023-03-15 22:32:34 +0100

    fix: fixed guest1 view

---
+++ SIMS-Initial-Project-main/InitialProject/InitialProject/Model/DTO/GuestAccommodationDTO.cs
@@ -0,0 +1,33 @@
+﻿using InitialProject.Resources.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitialProject.Model.DTO
+{
+    public  class GuestAccommodationDTO
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public AccommodationType Type { get; set; }
+        public int MaxGuests { get; set; }
+        public int MinReservationDays { get; set; }
+
+        public GuestAccommodationDTO()
+        {
+
+        }
+        public GuestAccommodationDTO(string name, string country, string city, AccommodationType type, int maxGuests, int minReservationDays)
+        {
+            Name = name;
+            Country = country;
+            City = city;
+            Type = type;
+            MaxGuests = maxGuests;
+            MinReservationDays = minReservationDays;
+        }
+    }
+}
--- SIMS-Initial-Project-main/InitialProject/InitialProject/View/Guest1/Guest1Window.xaml.cs
+++ SIMS-Initial-Project-main/InitialProject/InitialProject/View/Guest1/Guest1Window.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,21 +14,42 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using InitialProject.Model;
+using InitialProject.Repository;
+using InitialProject.Resources.Observer;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using InitialProject.Model.DTO;
 
 namespace InitialProject.View.Guest1
 {
     /// <summary>
     /// Interaction logic for Guest1Window.xaml
     /// </summary>
-    public partial class Guest1Window : Window
+    public partial class Guest1Window : Window, INotifyPropertyChanged, IObserver
     {
         public User LoggedInUser { get; set; }
+        public ObservableCollection<Accommodation> Accommodations { get; set; }
+        public ObservableCollection<GuestAccommodationDTO> AvailableAccommodations { get; set; }
+
+        private AccommodationRepository _accommodationRepository;
+
+        private LocationRepository _locationRepository;
 
         public Guest1Window(User user)
         {
             InitializeComponent();
             DataContext = this;
             LoggedInUser = user;
+
+            _accommodationRepository = new AccommodationRepository();
+            _accommodationRepository.Subscribe(this);
+
+            Accommodations = new ObservableCollection<Accommodation>(_accommodationRepository.GetAll());
+
+            _locationRepository = new LocationRepository();
+            _locationRepository.Subscribe(this);
+
+            AvailableAccommodations = new ObservableCollection<GuestAccommodationDTO>(ConvertToDTO(_accommodationRepository.GetAll()));
         }
 
         private void ReserveButton_Click(object sender, RoutedEventArgs e)
@@ -37,6 +60,40 @@ namespace InitialProject.View.Guest1
         private void searchBox_TextChanged(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+      
+        public void Update()
+        {
+            AvailableAccommodations.Clear();
+            foreach (Accommodation accommodation in _accommodationRepository.GetAll())
+            {
+                AvailableAccommodations.Add(ConvertToDTO(accommodation));
+            }
+        }
+        public List<GuestAccommodationDTO> ConvertToDTO(List<Accommodation> accommodations)
+        {
+            List<GuestAccommodationDTO> dto = new List<GuestAccommodationDTO>();
+            foreach (Accommodation accommodation in accommodations)
+            {
+                dto.Add(new GuestAccommodationDTO(accommodation.Name,
+                    _locationRepository.GetById(accommodation.LocationId).Country,
+                     _locationRepository.GetById(accommodation.LocationId).City,
+                     accommodation.Type, accommodation.MaxGuests, accommodation.MinReservationDays));
+            }
+            return dto;
+        }
+        public GuestAccommodationDTO ConvertToDTO(Accommodation accommodation)
+        {
+            return new GuestAccommodationDTO(accommodation.Name,
+                    _locationRepository.GetById(accommodation.LocationId).Country,
+                     _locationRepository.GetById(accommodation.LocationId).City,
+                       accommodation.Type, accommodation.MaxGuests, accommodation.MinReservationDays);
         }
     }
 }
