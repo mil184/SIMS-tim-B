d1bd598fd47532c05ad1abc014828bddb9235307
mil184 <djmilica127@gmail.com>
2023-04-07 18:14:13 +0200

    feat: voucher classes added

---
+++ SIMS-Initial-Project-main/InitialProject/InitialProject/Service/VoucherService.cs
@@ -0,0 +1,33 @@
+﻿using InitialProject.Model;
+using InitialProject.Repository;
+using InitialProject.Resources.Observer;
+using System;
+using System.Collections.Generic;
+
+namespace InitialProject.Service
+{
+    public class VoucherService : ISubject
+    {
+        private readonly VoucherRepository _voucherRepository;
+
+        public VoucherService()
+        {
+            _voucherRepository = new VoucherRepository();
+        }
+
+        public void Subscribe(IObserver observer)
+        {
+            _voucherRepository.Subscribe(observer);
+        }
+
+        public void Unsubscribe(IObserver observer)
+        {
+            _voucherRepository.Unsubscribe(observer);
+        }
+
+        public void NotifyObservers()
+        {
+            _voucherRepository.NotifyObservers();
+        }
+    }
+}
