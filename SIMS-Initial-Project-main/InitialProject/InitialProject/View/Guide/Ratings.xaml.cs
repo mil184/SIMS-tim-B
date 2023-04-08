9ee70d483080732ce0468e5a35f0eac457588c44
DeyoP <pelemisdejan6@gmail.com>
2023-04-08 16:16:42 +0200

    feat: added ratings window

---
+++ SIMS-Initial-Project-main/InitialProject/InitialProject/View/Guide/Ratings.xaml.cs
@@ -0,0 +1,32 @@
+﻿using InitialProject.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace InitialProject.View.Guide
+{
+    /// <summary>
+    /// Interaction logic for Ratings.xaml
+    /// </summary>
+    public partial class Ratings : Window
+    {
+        public Tour FinishedTour { get; set; }
+        public Ratings(Tour finishedTour)
+        {
+            InitializeComponent();
+            DataContext = this;
+            FinishedTour = finishedTour;
+        }
+
+    }
+}
