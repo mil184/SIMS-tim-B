a8f2c1efec22a1a6cefb3137954491e32514ff70
jelenaa01 <jecasubic@gmail.com>
2023-05-11 17:24:56 +0200

    feat: review of grades

---
+++ SIMS-Initial-Project-main/InitialProject/InitialProject/Model/DTO/Guest1RatingsDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitialProject.Model.DTO
+{
+    public class Guest1RatingsDTO
+    {
+        public int Cleanliness { get; set; }
+        public int Behavior { get; set; }
+        public string Comment { get; set; }
+
+        public Guest1RatingsDTO() { }
+
+        public Guest1RatingsDTO(int cleanliness, int behavior, string comment)  
+        {
+            Cleanliness = cleanliness;
+            Behavior = behavior;
+            Comment = comment;
+        }
+    }
+}
