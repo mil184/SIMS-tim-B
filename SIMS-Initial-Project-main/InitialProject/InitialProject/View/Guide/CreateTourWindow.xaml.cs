79f77488ba6b5862f77fee2a74fa23821fe76d9d
DeyoP <pelemisdejan6@gmail.com>
2023-05-19 20:19:03 +0200

    fix: new create window

---
+++ SIMS-Initial-Project-main/InitialProject/InitialProject/View/Guide/CreateTourWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace InitialProject.View.Guide
+{
+    /// <summary>
+    /// Interaction logic for CreateTourWindow.xaml
+    /// </summary>
+    public partial class CreateTourWindow : Window
+    {
+        public CreateTourWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCreateTour_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
